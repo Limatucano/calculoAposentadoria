d0cde247948896febc1892da732d936c0fb7cd75
Matheus Lima <matheus.correa@mindtek.com.br>
2021-02-09 21:57:26 -0300

    adicionando activity e calculo de idade

--- app/src/main/java/com/example/calculaaposentadoria/MainActivity.kt
+++ app/src/main/java/com/example/calculaaposentadoria/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.calculaaposentadoria
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity: Activity() {
+class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val spn_sexo = findViewById<Spinner>(R.id.spn_sexo)
+        val txt_idade = findViewById<EditText>(R.id.txt_idade)
+        val btn_calcular = findViewById<Button>(R.id.btn_calcular)
+        val txt_resultado = findViewById<TextView>(R.id.txt_resultado)
+
+
+        //Adicionando os valores para o spinner
+        spn_sexo.adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, listOf("masculino","feminino"))
+
+        btn_calcular.setOnClickListener {
+            val sexo = spn_sexo.selectedItem as String
+            val idade = txt_idade.text.toString().toInt()
+            var resultado = 0
+            if(sexo == "masculino"){
+                resultado = (65 - idade)
+            }else{
+                resultado = (60 - idade)
+            }
+            txt_resultado.text = "Faltam $resultado anos para você se aposentar."
+        }
+
 
     }
 }
